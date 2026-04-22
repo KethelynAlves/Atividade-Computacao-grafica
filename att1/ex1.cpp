d1e07064b27e2db4df59d5a1910efeee27a3035e
Kethelyn Alves <141461344+KethelynAlves@users.noreply.github.com>
2026-04-21 21:45:34 -0300

    Refactor to use GLFW and add GLUT for comparison

--- att1/ex1.cpp
+++ att1/ex1.cpp
@@ -1,6 +1,9 @@
 #include <GLAD/glad.h>
 #include <GLFW/glfw3.h>
+//#include <GL/glut.h>
 #include <cmath>
+
+//adicionei o GLUT para comparação do que muda entre as bibliotecas
 
 //Exercício 1: Mover um Círculo com o Teclado
 //Objetivo: Trocar o quadrado por um círculo e movimentá-lo com as teclas W, A, S, D.
@@ -20,6 +23,8 @@ void desenhaCena() {
         glVertex2f(jogadorX + (0.25f * cos(rad)*0.75f), jogadorY + 0.25f * sin(rad));
     }
     glEnd(); 
+
+    //glutSwapBuffers();
 }
 
 int main() {
@@ -54,3 +59,29 @@ int main() {
     glfwTerminate();
     return 0;
 }
+
+/*
+void keyboard(unsigned char key, int x, int y) {
+    if (key == 'w' || key == 'W') { if (jogadorY < limite) jogadorY += velocidade; }
+    if (key == 's' || key == 'S') { if (jogadorY > -limite) jogadorY -= velocidade; }
+    if (key == 'a' || key == 'A') { if (jogadorX > -limite) jogadorX -= velocidade; }
+    if (key == 'd' || key == 'D') { if (jogadorX < limite) jogadorX += velocidade; }
+    
+    glutPostRedisplay(); 
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Atividade GLUT - Kethelyn");
+
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    
+    glutDisplayFunc(desenhaCena);
+    glutKeyboardFunc(keyboard); // Registro da função de teclado
+
+    glutMainLoop();
+    return 0;
+}
+*/
